356d26f1c35040aa3a6b565f45c16e01097d53a4
Inho Kim <ikim0106@hotmail.com>
2017-10-17 17:05:30 +0800

    bad alarm system complete

--- commands/alarm/remind.js
+++ commands/alarm/remind.js
@@ -11,7 +11,7 @@ class remindMe extends commando.Command{
         super(client,
         {
             name:'remindme',
-            group:'alarm',
+            group:'alarm',  
             memberName:'alarm',
             description:'send a pm to the user with the given amount of time'
         });
@@ -19,8 +19,16 @@ class remindMe extends commando.Command{
 
     async run(message, args)
     {
-        message.reply(`remind ${args} ms`);
-        await sleep(args);
+        var hpos = args.indexOf('h')
+        var mpos = args.indexOf('m')
+        var spos = args.indexOf('s')
+        var hours = parseInt(args.substring(hpos-2,hpos))*3600000
+        var mins = parseInt(args.substring(mpos-2,mpos))*60000
+        var secs = parseInt(args.substring(spos-2,spos))*1000
+        var total=hours+mins+secs
+        console.log(total);
+        message.reply(`Noted!`);
+        await sleep(total);
         message.reply('TIME\'S UP!');
     }
 }
