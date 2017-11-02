af386ed9ab9243d80a333de8855e6bff28ed7532
Inho Kim <ikim0106@hotmail.com>
2017-11-02 14:59:47 +0800

    improved alarm system

--- commands/alarm/remind.js
+++ commands/alarm/remind.js
@@ -22,14 +22,38 @@ class remindMe extends commando.Command{
         var hpos = args.indexOf('h')
         var mpos = args.indexOf('m')
         var spos = args.indexOf('s')
+
+        var final = args.substring(spos+1)
+        console.log(final)
+
         var hours = parseInt(args.substring(hpos-2,hpos))*3600000
         var mins = parseInt(args.substring(mpos-2,mpos))*60000
         var secs = parseInt(args.substring(spos-2,spos))*1000
-        var total=hours+mins+secs
+        var total=0
+
+        if(hpos==null)
+        {
+            hours=0
+        }
+        else total+=hours
+
+        if(mpos==null)
+        {
+            mins=0
+        }
+        else total+=mins
+
+        if(spos==null)
+        {
+            secs=0
+        }
+        else total+=secs
+
         console.log(total);
-        message.reply(`Noted!`);
-        await sleep(total);
-        message.reply('TIME\'S UP!');
+
+        message.reply(`Noted!`)
+        await sleep(total)
+        message.reply(`TIME\'S UP! Here is the message you left for yourself:${final}`)
     }
 }
 
